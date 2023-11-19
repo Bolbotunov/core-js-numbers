6c000c77c408c55675d7a816dc9a7c58f3cb9aa7
Bolbotunov <100226065+Bolbotunov@users.noreply.github.com>
2023-11-19 20:32:48 +0300

    fix: isPrime add +=

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -200,7 +200,7 @@ function isPrime(n) {
   if (n < 2) {
     return false;
   }
-  for (let i = 2; i < n; i++) {
+  for (let i = 2; i < n; i += 1) {
     if (n % i === 0) {
       return false;
     }
