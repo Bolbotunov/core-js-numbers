03cdfe2fffbd6b43a2043bb1395022abb4462603
Bolbotunov <100226065+Bolbotunov@users.noreply.github.com>
2023-11-19 20:31:35 +0300

    fix: isPrime add some code

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -197,8 +197,13 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-  if (n % 2 === 0 && n !== 2) {
+  if (n < 2) {
     return false;
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false;
+    }
   }
   return true;
 }
