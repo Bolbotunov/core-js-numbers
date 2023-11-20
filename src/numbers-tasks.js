d08226c0124c1ee598e5b703fc304bb14b29fd6b
Bolbotunov <100226065+Bolbotunov@users.noreply.github.com>
2023-11-20 15:28:24 +0300

    feat: roundToNearestInteger & getIntegerPartNumber solved

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -536,8 +536,8 @@ function roundToLargestInteger(number) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function roundToNearestInteger(/* number */) {
-  throw new Error('Not implemented');
+function roundToNearestInteger(number) {
+  return Math.round(number);
 }
 
 /**
@@ -551,8 +551,8 @@ function roundToNearestInteger(/* number */) {
  * 5.4  => 5
  * -5.5 => -5
  */
-function getIntegerPartNumber(/* number */) {
-  throw new Error('Not implemented');
+function getIntegerPartNumber(number) {
+  return Math.trunc(number);
 }
 
 /**
